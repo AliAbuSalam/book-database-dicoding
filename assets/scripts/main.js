ad2ebf53ccbb92a90de5fba2d86f9b07f9fe7a91
Ali Abu Salam <aliadadehmav@gmail.com>
2022-10-10 20:18:09 +0700

    add initial js script and add feature to check whether add form is empty or not

---
+++ assets/scripts/main.js
@@ -0,0 +1,30 @@
+const BOOK_LIST_KEY = 'book_list_key';
+
+const addBookForm = document.getElementById('addBookForm');
+const judulInput = document.getElementById('judulInput');
+const penulisInput = document.getElementById('penulisInput');
+const tahunInput = document.getElementById('tahunInput');
+const selesaiDibacaCheckbox = document.getElementById('alreadyReadCheckbox');
+
+function loadInitialBookList(){
+  if(typeof (Storage) === 'undefined'){
+    alert('Browser anda tidak mendukung web storage!');
+    return;
+  }
+  return JSON.parse(localStorage.getItem(BOOK_LIST_KEY)) || [];
+}
+
+let bookList = loadInitialBookList();
+
+addBookForm.addEventListener('submit', function(event){
+  event.preventDefault();
+
+  console.log('form submitted');
+});
+
+addBookForm.addEventListener('change', function(){
+  const addBookButton = document.getElementById('tambah');
+
+  if(judulInput.value && penulisInput.value && tahunInput.value) addBookButton.removeAttribute('disabled');
+  else addBookButton.setAttribute('disabled', true);
+})
